64048b687a34958f9b41813826d496a38ddd7e23
kelvin jose hernandez cabrera <kelhzkb@hotmail.com>
2025-05-15 06:55:00 -0500

    Add programs

--- src/data/programas.ts
+++ src/data/programas.ts
@@ -1,14 +1,29 @@
 const programas = [
-  'Ingenieria de sistemas',
-  'Economia',
-  'Medicina',
-  'Odontologia',
-  'Quimica',
-  'Filosofia',
-  'Derecho',
-  'Administracion de empresas',
-  'Administracion industrial',
-  'Otras',
+  "Administración de Empresas (Diurna y Nocturna)",
+  "Administración Industrial",
+  "Contaduría Pública (Diurna y Nocturna)",
+  "Economía",
+  "Biología",
+  "Química",
+  "Matemáticas",
+  "Técnica Profesional en Procesos Metrológicos",
+  "Tecnología en Metrología Industrial",
+  "Comunicación Social",
+  "Trabajo Social",
+  "Licenciatura en Educación con Énfasis en Ciencias Sociales y Ambientales",
+  "Licenciatura en Educación Infantil",
+  "Derecho",
+  "Ingeniería Civil",
+  "Ingeniería de Alimentos",
+  "Ingeniería de Sistemas (Diurna y Nocturna)",
+  "Ingeniería Química",
+  "Lenguas Extranjeras con énfasis en Inglés y Francés",
+  "Historia",
+  "Filosofía",
+  "Lingüística y Literatura",
+  "Profesional Universitario en Lenguas Extranjeras"
 ];
 
 export default programas;
+
+
